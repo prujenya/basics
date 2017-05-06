ca325b5dae6551b3bcc42c1e5675bc71bcbc3794
prujenya <y.mamasakhlisov@gmail.com>
2017-05-06 18:13:54 +0400

    6may2017 update

--- src/com/company/firstlevel/Main.java
+++ src/com/company/firstlevel/Main.java
@@ -1,6 +1,7 @@
 package com.company.firstlevel; //package name: provides the unique namespace for the application
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.out;
 import java.io.*;
@@ -14,91 +15,39 @@ public class Main {
 	// write your code here
         out.println("Welcome to Armenian - Indian training center!");// method "System.out.println": doing something
 
+        MyRecord rc1 = new MyRecord("Ashot", "Grigoryan", "094222211", "1 Mannogian St., 0025 Yerevan, Armenia", "ashot@gmail.com", "www.ysu.ws");
+        MyRecord rc2 = new MyRecord("Hovhannes", "Gasparyan", "093222233", "2 Mannogian St., 0025 Yerevan, Armenia", "hovog@gmail.com", "www.ysu.ws");
+
+        MyRecord[] mrcarray = {rc1, rc2};
+        System.out.println("test: " + mrcarray[1].getPhone());
+
+        MyAddressBook mab = new MyAddressBook(mrcarray);
+        //System.out.println("test: " + mab.getRecords()[0].getEmail());
+
+        String myfilename1 = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile3.txt";
+
+        MyAddressBook mab1 = new MyAddressBook();
+        mab1.addRecord(rc1, myfilename1);
+        mab1.getRecords()[0].getName();
+        System.out.println("test: " + mab1.getRecords()[0].getEmail());
+
+        ArrayList<String> al;
+        al = new ArrayList<String>(5);
+        al.add("wejkhkwjehk");
+        al.add("wexx485798479e8798ehk");
+        System.out.println("size: " + al.size());
+
+
+
         //Scanner in = new Scanner(System.in);
 
         //int number0 = in.nextInt();
         //long number = in.nextLong();
 
-        int i;
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
-        FileOutputStream fos1 = null;
-
-        String filename = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile1.txt";
-        String myfilename = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile4.txt";
-        String myfilename1 = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile3.txt";
-
-
-        String str;
-        str = "ZZZZZZZZZsdlskdlskdjlksdjlkfsjldkfXXXXXXXXXXXXXXXXXXXXXX\n";
-
-        byte[] bstr = str.getBytes();
-
-
-
-            try {
-
-                fis = new FileInputStream(filename);
-                fos = new FileOutputStream(myfilename);
-                fos1 = new FileOutputStream(myfilename1,true);
-
-                fos1.write(bstr);
-
-                do {
-                    i = fis.read();
-                    if (i != -1) {
-
-                        System.out.print("." + (char) i + ".");
-                        System.out.print("." + i + ".");
-                        fos.write(i);
-                    }
-
-
-                } while (i != -1);
-
-
-            } catch (FileNotFoundException ex) {
-
-                System.out.println("file is not found");
-
-            } catch (IOException ex1) {
-
-                System.out.println("i/o error");
-
-            } catch (NullPointerException ex2) {
-
-                System.out.println(ex2.toString());
-
-            } finally {
-
-                try {
-
-                    if (fis != null)
-                        fis.close();
-
-                } catch (IOException e) {
-
-                    System.out.println("file closing error");
-                }
-
-                try {
-
-                    if (fos != null)
-                        fos.close();
-
-                } catch (IOException e) {
-
-                    System.out.println("file closing error");
-                }
-            }
-
-
-
 
 
 
     }
-
 
 
     }
--- src/com/company/firstlevel/MyAddressBook.java
+++ src/com/company/firstlevel/MyAddressBook.java
@@ -1,5 +1,7 @@
 package com.company.firstlevel;
 
+import java.io.*;
+
 /**
  * Created by Studenta on 04.05.2017.
  */
@@ -13,10 +15,10 @@ public class MyAddressBook {
 
     }
 
-    public MyAddressBook(int nbr, MyRecord[] rcd){
+    public MyAddressBook(MyRecord[] rcd){
 
-        this.recordsnumber = nbr;
-        this.records = new MyRecord[nbr];
+        this.recordsnumber = rcd.length;
+        this.records = new MyRecord[rcd.length];
 
         this.records = rcd;
         /*
@@ -30,6 +32,28 @@ public class MyAddressBook {
         this.records = records;
     }
 
+    public MyRecord[] createRecords(){
+
+        MyRecord[] mrc = null;
+        ////////////////////////////
+
+
+        ////////////////////////////
+        return mrc;
+    }
+
+    public String[] getLinesFromFile(String filename){ // private ??
+
+        String[] str = null;
+        ////////////////////////////
+
+
+        ////////////////////////////
+        return str;
+    }
+
+
+
     public MyRecord[] getRecords() {
         return records;
     }
@@ -42,12 +66,97 @@ public class MyAddressBook {
         return recordsnumber;
     }
 
-    public void addRecord(){
+    public void addRecord(MyRecord myrecord, String filestorage){
+
+        FileOutputStream fos1 = null;
+        String str = "*" + myrecord.getName() + "*";
+
+        this.records = new MyRecord[1];
+        this.records[0] = myrecord;
+
+        /*
+        int i;
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        FileOutputStream fos1 = null;
+
+        String filename = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile1.txt";
+        String myfilename = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile4.txt";
+        String myfilename1 = "C:\\Users\\Studenta.AITC\\IdeaProjects\\mybasics\\src\\com\\company\\firstlevel\\myfile3.txt";
+
+
+        String str;
+        str = "ZZZZZZZZZsdlskdlskdjlksdjlkfsjldkfXXXXXXXXXXXXXXXXXXXXXX\n";
+
+        byte[] bstr = str.getBytes();
+
+
+
+            try {
+
+                fis = new FileInputStream(filename);
+                fos = new FileOutputStream(myfilename);
+                fos1 = new FileOutputStream(myfilename1,true);
+
+                fos1.write(bstr);
+
+                do {
+                    i = fis.read();
+                    if (i != -1) {
+
+                        System.out.print("." + (char) i + ".");
+                        System.out.print("." + i + ".");
+                        fos.write(i);
+                    }
+
+
+                } while (i != -1);
+
+
+            } catch (FileNotFoundException ex) {
+
+                System.out.println("file is not found");
+
+            } catch (IOException ex1) {
+
+                System.out.println("i/o error");
+
+            } catch (NullPointerException ex2) {
+
+                System.out.println(ex2.toString());
+
+            } finally {
+
+                try {
+
+                    if (fis != null)
+                        fis.close();
+
+                } catch (IOException e) {
+
+                    System.out.println("file closing error");
+                }
+
+                try {
+
+                    if (fos != null)
+                        fos.close();
+
+                } catch (IOException e) {
+
+                    System.out.println("file closing error");
+                }
+            }
+
+
+
+
+*/
 
     }
 
     public void removeRecord(int recordid){
 
-        
+
     }
 }
